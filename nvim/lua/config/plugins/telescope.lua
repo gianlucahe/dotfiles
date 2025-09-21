c917e3e588945a6086b1bfbf3c5ef5f1dd612c46
gianlucahe <154693320+gianlucahe@users.noreply.github.com>
2025-09-21 16:49:18 +0200

    chore: add tmux, opencode, nvim-plugins/trouble, fugitivive, notify

---
+++ nvim/lua/config/plugins/telescope.lua
@@ -0,0 +1,52 @@
+return {
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    },
+    config = function()
+      require('telescope').setup {
+        defaults = {
+          border = true,
+          layout_config = {
+            height = 0.9,
+            width = 0.9,
+            preview_cutoff = 0,
+            horizontal = { preview_width = 0.60 },
+            vertical = { width = 0.9, height = 0.9, preview_cutoff = 0 },
+          },
+        },
+        pickers = {
+          find_files = {
+            layout_config = {
+              height = 0.9,
+              width = 0.9,
+            },
+          }
+        },
+        extensions = {
+          fzf = {}
+        }
+      }
+
+      require('telescope').load_extension('fzf')
+
+      vim.keymap.set("n", "<space>fh", require('telescope.builtin').help_tags)
+      vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
+      vim.keymap.set("n", "<space>en", function()
+        require('telescope.builtin').find_files {
+          cwd = vim.fn.stdpath("config")
+        }
+      end)
+      vim.keymap.set("n", "<space>ep", function()
+        require('telescope.builtin').find_files {
+          cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
+        }
+      end)
+
+      require "config.telescope.multigrep".setup()
+    end
+  }
+}
